4cc5a237aafbf15f19c4f7c73b8f8349c62cf07a
osilla <osilla@mercadona.es>
2023-02-27 19:14:07 +0100

    feat: Rename articles files to lastNew

---
+++ src/containers/home/events/index.tsx
@@ -0,0 +1,21 @@
+
+
+import React from 'react'
+import Title from '../../../components/title'
+
+const Events = () => {
+  return (
+    <div>
+        <div className="last-news">
+      <div className="jcc">
+        <Title title="Eventos" />
+      </div>
+      <div className="list">
+        
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Events
